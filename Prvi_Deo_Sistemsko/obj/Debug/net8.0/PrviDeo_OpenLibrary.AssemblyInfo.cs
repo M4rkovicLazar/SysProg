1732581d354245d09560080d53cd4c8fe07c5c40
Lazar Markovic <markoviclaki03.4@gmail.com>
2025-09-30 23:24:06 +0200

    Sistemsko

--- Prvi_Deo_Sistemsko/obj/Debug/net8.0/PrviDeo_OpenLibrary.AssemblyInfo.cs
+++ Prvi_Deo_Sistemsko/obj/Debug/net8.0/PrviDeo_OpenLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrviDeo_OpenLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3e101083e3ac6be993bfdbf8dc44b320bd531f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrviDeo_OpenLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrviDeo_OpenLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
